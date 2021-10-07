35d32027a5dcf86ea6e8393e1c9acea108493891
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-07 17:37:27 -0300

    query e resolver

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-import { hello } from './module'
-console.log(hello())
+import { ApolloServer, gql } from 'apollo-server'
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'Olá Mundo!'
+      },
+    },
+  },
+})
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on url ${url}`)
+})
