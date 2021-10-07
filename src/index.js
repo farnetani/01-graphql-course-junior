6d614989d40592b068542ae3a658b141bde50a65
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-07 17:47:19 -0300

    Types Scalar

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import { ApolloServer, gql } from 'apollo-server'
 
+// Types Scalar: ID, String, Int, Float, Boolean
+// Por padrão vem null, se nao quiser, tem que por !
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       id: ID
-      name: String
+      name: String!
+      age: Int
+      salary: Float
+      married: Boolean!
     }
   `,
   resolvers: {
     Query: {
       id: () => 1,
       name: () => 'Junior',
+      age: () => 30,
+      salary: () => 2000.5,
+      married: () => false,
     },
   },
 })
