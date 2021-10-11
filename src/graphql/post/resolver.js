f2b8ae2b56660dc8cc876d7545330d1fd7069623
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-10 23:25:29 -0300

    union types

--- src/graphql/post/resolver.js
+++ src/graphql/post/resolver.js
@@ -1,24 +1,36 @@
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id)
-  return response.json()
+  const post = await response.json()
+
+  if (typeof post.id === 'undefined') {
+    return {
+      statusCode: 404,
+      message: 'Post not found!',
+    }
+  }
+
+  return post
 }
 
 const posts = async (_, { input }, { getPosts }) => {
   const apiFiltersInput = new URLSearchParams(input)
-  // console.log(apiFiltersInput.toString())
   const response = await getPosts('/?' + apiFiltersInput)
   return response.json()
 }
 
 export const postResolvers = {
-  Query: {
-    post,
-    posts,
-  },
+  Query: { post, posts },
   Post: {
     unixTimestamp: ({ createdAt }) => {
       const timestamp = new Date(createdAt).getTime() / 1000
       return Math.floor(timestamp)
+    },
+  },
+  PostResult: {
+    __resolveType: (obj) => {
+      if (typeof obj.statusCode !== 'undefined') return 'PostNotFoundError'
+      if (typeof obj.id !== 'undefined') return 'Post'
+      return null
     },
   },
 }
