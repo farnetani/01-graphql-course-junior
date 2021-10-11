0afbcce96b8c8187c912d407a77ccd01be6fcb4f
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-10 22:01:26 -0300

    29 - Inputs

--- queries/queries_0006.sql
+++ queries/queries_0006.sql
@@ -1,12 +1,49 @@
 {
   posts(input: {
-    _sort: "inputRef"
+    _sort: "indexRef"
     _order:"desc"
     _start: 0
     _limit: 2
   }) {
+    indexRef
     title
     createdAt
     unixTimestamp
+  }
+}
+
+
+{
+  users(input: {
+    _sort: "indexRef"
+    _order:"desc"
+    _start: 0
+    _limit: 2
+  }) {
+    indexRef
+    id
+    firstName
+    lastName
+    userName
+    createdAt
+  }
+}
+
+## GET_USERS
+query GET_USERS(
+  $sort: String = "indexRef"
+  $order: String = "desc"
+  $start: Int = 0
+  $limit: Int = 5
+) {
+  users(
+    input: { _sort: $sort, _order: $order, _start: $start, _limit: $limit }
+  ) {
+    indexRef
+    id
+    firstName
+    lastName
+    userName
+    createdAt
   }
 }
