3a89a24526417962b4f268f90873e7953fa93fdd
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-12 16:52:10 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ApolloServer } from 'apollo-server'
 import { resolvers, typeDefs } from './graphql/schema'
 import { context } from './graphql/context'
+import { PostsApi } from './graphql/post/datasources'
 
 // Types Scalar: ID, String, Int, Float, Boolean
 // Por padrão vem null, se nao quiser, tem que por !
@@ -9,7 +10,12 @@ const server = new ApolloServer({
   typeDefs,
   // resolvers: resolvers,
   resolvers,
-  context: context,
+  context,
+  dataSources: () => {
+    return {
+      postApi: new PostsApi(),
+    }
+  },
 })
 
 server.listen(4007).then(({ url }) => {
