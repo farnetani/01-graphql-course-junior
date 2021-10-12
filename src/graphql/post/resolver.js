0d0f5a2bf89166d5ca29b76a40c180240530f779
Arlei F. Farnetani Junior <farneetani@gmail.com>
2021-10-12 17:18:38 -0300

    cache com timer

--- src/graphql/post/resolver.js
+++ src/graphql/post/resolver.js
@@ -1,95 +1,18 @@
-// const post = async (_, { id }, { getPosts }) => {
-//   const response = await getPosts('/' + id)
-//   const post = await response.json()
-
-//   if (Math.random() > 0.5) {
-//     return {
-//       statusCode: 500,
-//       message: 'Post timeout!',
-//       timeout: 123,
-//     }
-//   }
-
-//   if (typeof post.id === 'undefined') {
-//     return {
-//       statusCode: 404,
-//       message: 'Post not found!',
-//       postId: id,
-//     }
-//   }
-
-//   return post
-// }
-
 const post = async (_, { id }, { dataSources }) => {
   const post = dataSources.postApi.getPost(id)
-
-  if (Math.random() > 0.5) {
-    return {
-      statusCode: 500,
-      message: 'Post timeout!',
-      timeout: 123,
-    }
-  }
-
-  if (typeof post.id === 'undefined') {
-    return {
-      statusCode: 404,
-      message: 'Post not found!',
-      postId: id,
-    }
-  }
-
   return post
 }
-
-// const posts = async (_, { input }, { getPosts }) => {
-//   const apiFiltersInput = new URLSearchParams(input)
-//   const response = await getPosts('/?' + apiFiltersInput)
-//   return response.json()
-// }
-
-// // const user = async (parent) => {
-// // const user = async ({ userId }, _, { getUsers }) => {
-// const user = async ({ userId }, _, { userDataLoader }) => {
-//   // Dataloader
-//   return userDataLoader.load(userId)
-// }
 
 const posts = async (_, { input }, { dataSources }) => {
   const posts = dataSources.postApi.getPosts(input)
   return posts
 }
 
-// const user = async (parent) => {
-// const user = async ({ userId }, _, { getUsers }) => {
 const user = async ({ userId }, _, { userDataLoader }) => {
-  // Dataloader
   return userDataLoader.load(userId)
 }
 
 export const postResolvers = {
   Query: { post, posts },
-  Post: {
-    unixTimestamp: ({ createdAt }) => {
-      const timestamp = new Date(createdAt).getTime() / 1000
-      return Math.floor(timestamp)
-    },
-    user: user,
-  },
-  PostResult: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError'
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError'
-      if (typeof obj.id !== 'undefined') return 'Post'
-      return null
-    },
-  },
-  PostError: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError'
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError'
-      return null
-    },
-  },
+  Post: { user },
 }
